056ad3d829faf9ed4d56ab7ef08bbc01d45daf0a
Dennis Molla <crackeddennism12@gmail.com>
2026-05-10 13:16:05 +0300

    Add teacher constructor and improve comments

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
 int main() {
 	//student constructor (name , id , semester , gender(bool)
 	Student s1("George", "student_id_here001", 1, 0);
-	
+	//teacher constructor (name, password, gender(bool), profession
 	Teacher t1("Acelot", "password123", 0, 1);
-
 	//subject constructor (name, description, semester, course leader(WIP)
 	Subject subject("adawd0","ada", 1 , 1);
+
+	//add course to student vector
 	s1.AddCourse(subject);
 
     return 0;
