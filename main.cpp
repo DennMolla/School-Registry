9c7db5971df4f2522562b18cc8a2ad62f4c47855
Dennis Molla <crackeddennism12@gmail.com>
2026-06-21 10:29:16 +0300

    Refactor main.cpp to integrate GUI and Registry

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
-#include <iostream>
-#include "student.h"
-#include "teacher.h"
-#include "subject.h"
-
+#include "ui.h"
 int main() {
-	//student constructor (name , id , semester , gender(bool)
-	Student s1("George", "student_id_here001", 1, 0);
-	//teacher constructor (name, password, gender(bool), profession
-	Teacher t1("Acelot", "password123", 0, 1);
-	//subject constructor (name, description, semester, course leader(WIP)
-	Subject subject("adawd0","ada", 1 , 1);
+	Registry r;
 
-	//add course to student vector
-	s1.AddCourse(subject);
+	GUI ui(r);
+	ui.InitNC();
+
+	ui.RunMenu();
 
     return 0;
 }
