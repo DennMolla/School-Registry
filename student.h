c1edf7f9006ac7a2e35503f3beff37c66c4dbb01
Dennis Molla <crackeddennism12@gmail.com>
2026-06-21 10:31:03 +0300

    Refactor Student class and update method signatures
    Refactor Student class to improve readability and structure.

--- student.h
+++ student.h
@@ -2,24 +2,26 @@
 #define student_h
 
 
-
 #include <string>
 //#include <cstring>
-#include <vector>
+//#include <vector>
 #include "person.h"
+
 
 class Subject;
 
-class Student:public Person{
+class Student :public Person {
 private:
     char* AM;
     unsigned int semester;
-	
-	std::vector<Subject> courses;
+
+   // std::vector<Subject> courses;
 
 public:
-	void AddCourse(const Subject& course);
-	
+    
+
+    //void AddCourse(const Subject& course);
+
     // constructors & destructor
     Student(std::string nName, const char* nAM);
     Student(std::string nName, const char* nAM, unsigned int nSemester, bool nGender);
@@ -29,18 +31,22 @@ public:
     // operator overloads
     Student& operator++();
     Student operator++(int);
-    
+
     Student& operator+=(unsigned int);
     Student& operator-=(unsigned int);
     Student& operator=(const Student& s);
 
-    void print(std::ostream& ss) const;
+    void print(std::ostream& ss) const override;
 
     // setters
     void amSetter(const char* nAM);
+    // void nameSetter(std::string nName);
+   //  void genderSetter(bool nGender);
     void semesterSetter(unsigned int nSem);
 
     // getters
+  //  std::string getGender() const;
+   // std::string getName() const;
     unsigned int getSemester() const;
     const char* amGetter() const;
 };
