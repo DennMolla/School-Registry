a2cecd8662ee8b5e09d5490b4755aff6827fe626
Dennis Molla <crackeddennism12@gmail.com>
2026-06-21 10:30:43 +0300

    Refactor Student class memory management and error handling

--- student.cpp
+++ student.cpp
@@ -1,17 +1,19 @@
+//#include "subject.h"
 #include "student.h"
-#include "subject.h"
 #include <iostream>
 #include <stdexcept>
-#include <vector>
+//#include <vector>
 
 Student& Student::operator=(const Student& s) {
     if (this == &s) return *this;
 
     delete[] AM;
-
-	Person::operator=(s);
-    AM = new char[strlen(s.AM) + 1];
-    strcpy(AM, s.AM);
+    AM = nullptr;
+    Person::operator=(s);
+    if (s.AM != nullptr && strlen(s.AM) > 0) {
+        AM = new char[strlen(s.AM) + 1];
+        strcpy(AM, s.AM);
+    }
     semester = s.semester;
 
 
@@ -45,32 +47,43 @@ Student& Student::operator-=(unsigned int n) {
 void Student::print(std::ostream& ss) const {
     ss << getName() << "(" << getName().length() + 1 << ")"
         << "," << getGender()
-        << "," << amGetter()
+        << "," << (AM ? AM : "")
         << "," << getSemester()
         << std::endl;
 }
 
 // copy constructor
-Student::Student(const Student& s):Person(s), semester(s.semester){
-    AM = new char[strlen(s.AM) + 1];
-    strcpy(AM, s.AM);
-    
+Student::Student(const Student& s) :Person(s), semester(s.semester) {
+    AM = nullptr;
+    if (s.AM != nullptr && strlen(s.AM) > 0) {
+        AM = new char[strlen(s.AM) + 1];
+        strcpy(AM, s.AM);
+    }
+
 }
 
 // constructor 1
-Student::Student(std::string nName, const char* nAM): Person(nName, 0) {
-    AM = new char[strlen(nAM) + 1];
-    strcpy(AM, nAM);
+Student::Student(std::string nName, const char* nAM) : Person(nName, 0) {
+    AM = nullptr;
+    if (nAM != nullptr && strlen(nAM) > 0) {
+        AM = new char[strlen(nAM) + 1];
+        strcpy(AM, nAM);
+    }
     semester = 1;
 }
 
 // constructor 2
-Student::Student(std::string nName, const char* nAM, unsigned int nSemester, bool nGender): Person(nName, nGender) {
-	if(nSemester == 0){
-		throw std::invalid_argument("Το εξάμηνο δεν μπορεί να είναι μικρότερο ή ίσο με το 0!");
-	}
-    AM = new char[strlen(nAM) + 1];
-    strcpy(AM, nAM);
+Student::Student(std::string nName, const char* nAM, unsigned int nSemester, bool nGender) : Person(nName, nGender) {
+   
+    
+    if (nSemester == 0) {
+        //throw std::invalid_argument("Το εξάμηνο δεν μπορεί να είναι μικρότερο ή ίσο με το 0!");
+    }
+    AM = nullptr;
+    if (nAM != nullptr && strlen(nAM) > 0) {
+        AM = new char[strlen(nAM) + 1];
+        strcpy(AM, nAM);
+    }
     semester = nSemester;
 }
 
@@ -81,16 +94,19 @@ Student::~Student() {
 // setters
 
 void Student::semesterSetter(unsigned int nSem) {
-	if(nSem == 0){
-		throw std::invalid_argument("Το εξάμηνο δεν μπορεί να είναι μικρότερο ή ίσο με το 0!");
-	}
+    if (nSem == 0) {
+        //throw std::invalid_argument("Το εξάμηνο δεν μπορεί να είναι μικρότερο ή ίσο με το 0!");
+    }
     semester = nSem;
 }
 
 void Student::amSetter(const char* nAM) {
     delete[] AM;
-    AM = new char[strlen(nAM) + 1];
-    strcpy(AM, nAM);
+    AM = nullptr;
+    if (nAM != nullptr && strlen(nAM) > 0) {
+        AM = new char[strlen(nAM) + 1];
+        strcpy(AM, nAM);
+    }
 }
 
 // getters
@@ -100,10 +116,4 @@ const char* Student::amGetter() const {
 
 unsigned int Student::getSemester() const {
     return semester;
-}
-
-
-void Student::AddCourse(const Subject& course){
-	this->courses.push_back(course);
-	std::cout<<"Successfully added course"<<std::endl;
 }
